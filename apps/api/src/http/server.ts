47b30a4439bda8c1b8e9cb05eddc53a9764423fe
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-01-08 20:00:45 -0300

    feat(api): add server config

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,38 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { createAccount } from './routes/auth/create-account'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next Saas RBAC',
+      description: 'Next Saas RBAC API with multi-tenant',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors)
+
+app.register(createAccount)
+
+app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running'))
