f1f4b45131e01fdb2779587aa1bf7fa8f4d0f061
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-01-08 20:00:20 -0300

    feat(api): add controller create-account

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,46 @@
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Create a new account',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: 'User with same email already exists!',
+        })
+      }
+      const passwordHash = await hash(password, 6)
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      reply.status(201).send({ user })
+    },
+  )
+}
