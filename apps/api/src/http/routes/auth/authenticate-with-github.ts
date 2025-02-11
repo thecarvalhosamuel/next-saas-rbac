b094d87178c47861d0889024565dc8544bd2c1f3
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-02-10 22:00:14 -0300

    feat(api): add authenticateWithGithub

---
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -0,0 +1,134 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+import { environment } from '@/shared/constants/env'
+
+import { BadRequestError } from '../_errors/bad-request'
+
+export async function authenticateWithGithub(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/github',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Authenticate with github',
+        body: z.object({
+          code: z.string(),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code } = request.body
+
+      const githubOAuthUrl = new URL(
+        'https://github.com/login/oauth/access_token',
+      )
+      // 'https://github.com/login/oauth/authorize?client_id=Ov23liJEE3rW8l9yebPH&redirect_uri=http://localhost:3000/api/auth/callback&scope=user:email'
+
+      githubOAuthUrl.searchParams.set('client_id', environment.GITHUB_CLIENT_ID)
+      githubOAuthUrl.searchParams.set(
+        'client_secret',
+        environment.GITHUB_SECRET,
+      )
+      githubOAuthUrl.searchParams.set(
+        'redirect_uri',
+        'http://localhost:3000/api/auth/callback',
+      )
+      githubOAuthUrl.searchParams.set('code', code)
+
+      const githubAccessTokenResponse = await fetch(githubOAuthUrl, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      const githubAccessTokenData = await githubAccessTokenResponse.json()
+
+      console.log(githubAccessTokenData)
+
+      const { access_token: githubAccessToken } = z
+        .object({
+          access_token: z.string(),
+          token_type: z.literal('bearer'),
+          scope: z.string(),
+        })
+        .parse(githubAccessTokenData)
+
+      const githubUserResponse = await fetch('https://api.github.com/user', {
+        headers: {
+          Authorization: `Bearer ${githubAccessToken}`,
+        },
+      })
+
+      const githubUserData = await githubUserResponse.json()
+
+      console.log(githubUserData)
+      const {
+        id: githubId,
+        name,
+        email,
+        avatar_url: avatarUrl,
+      } = z
+        .object({
+          id: z.number().int().transform(String),
+          avatar_url: z.string().url(),
+          name: z.string().nullable(),
+          email: z.string().nullable(),
+        })
+        .parse(githubUserData)
+
+      if (email === null) {
+        throw new BadRequestError(
+          'Your Github account must have an email to authenticate.',
+        )
+      }
+
+      let user = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!user) {
+        user = await prisma.user.create({
+          data: {
+            name,
+            email,
+            avatarUrl,
+          },
+        })
+      }
+
+      let account = await prisma.account.findUnique({
+        where: {
+          provider_userId: {
+            provider: 'GITHUB',
+            userId: user.id,
+          },
+        },
+      })
+
+      if (!account) {
+        account = await prisma.account.create({
+          data: {
+            provider: 'GITHUB',
+            provideAccountId: githubId,
+            userId: user.id,
+          },
+        })
+      }
+
+      const token = app.jwt.sign({ sub: user.id }, { expiresIn: '7d' })
+
+      console.log(token)
+
+      reply.send()
+    },
+  )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -11,6 +11,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { errorHandler } from './errorHandler'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
@@ -49,5 +50,6 @@ app.register(authenticateWithPassword)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
+app.register(authenticateWithGithub)
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running'))
