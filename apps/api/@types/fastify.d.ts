75aaa84ac182a8887adaa57329b3b3210447a062
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-03-16 19:27:23 -0300

    feat: auth getMembership

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,11 @@
 import 'fastify'
 
+import { type Member, type Organization } from '@prisma/client'
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string,
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance, FastifyRequest } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnautorizedError } from '../routes/_errors/unauthorizedError'
 
 export const auth = fastifyPlugin(async (app: FastifyInstance) => {
@@ -10,9 +12,22 @@ export const auth = fastifyPlugin(async (app: FastifyInstance) => {
         const { sub } = await request.jwtVerify<{ sub: string }>()
         return sub
       } catch (error) {
-        throw new UnautorizedError('Invalid auth token')
+        throw new UnautorizedError('Invalid auth token.')
       }
     }
-    await request.jwtVerify()
+
+    request.getUserMembership = async (slug: string) => {
+      const userId = await request.getCurrentUserId()
+      const member = await prisma.member.findFirst({
+        where: { userId, organization: { slug } },
+        include: { organization: true },
+      })
+      if (!member) {
+        throw new UnautorizedError('You are not a member of this organization.')
+      }
+
+      const { organization, ...membership } = member
+      return { organization, membership }
+    }
   })
 })
