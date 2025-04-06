330bf9d9201961ae67d9e212f7a5af5d7a49fe53
Samuel Carvalho <carvalho.asamuel@gmail.com>
2025-04-06 15:27:02 -0300

    feat: add shadcn to web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='pt-BR' className='dark'>
       <body>{children}</body>
     </html>
   )
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
